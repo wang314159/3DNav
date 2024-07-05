dd740ecbaf26a6da2423e6121756cc75a4372a60
wang314159 <2516901236@qq.com>
2024-07-05 19:16:03 +0800

    updating replay buffer

--- rl/raisimGymTorch/algo/elegantrl/replay_buffer.py
+++ rl/raisimGymTorch/algo/elegantrl/replay_buffer.py
@@ -7,6 +7,160 @@ from torch import Tensor
 from .config import Config
 
 
+import numpy as np
+
+# class ReplayBuffer:  # for off-policy
+#     def __init__(self,
+#                  max_size: int,
+#                  state_dim: int,
+#                  action_dim: int,
+#                  gpu_id: int = 0,
+#                  num_seqs: int = 1,
+#                  if_use_per: bool = False,
+#                  args: Config = Config()):
+#         self.p = 0  # pointer
+#         self.if_full = False
+#         self.cur_size = 0
+#         self.add_size = 0
+#         self.add_item = None
+#         self.max_size = max_size
+#         self.num_seqs = num_seqs
+#         self.device = 'cpu'  # Numpy does not use GPU
+#         self.states = np.empty((max_size, num_seqs, state_dim), dtype=np.float32)
+#         self.actions = np.empty((max_size, num_seqs, action_dim), dtype=np.float32)
+#         self.rewards = np.empty((max_size, num_seqs), dtype=np.float32)
+#         self.undones = np.empty((max_size, num_seqs), dtype=np.float32)
+#         self.if_use_per = if_use_per
+#         if if_use_per:
+#             self.sum_trees = [SumTree(buf_len=max_size) for _ in range(num_seqs)]
+#             self.per_alpha = getattr(args, 'per_alpha', 0.6)
+#             self.per_beta = getattr(args, 'per_beta', 0.4)
+#         else:
+#             self.sum_trees = None
+#             self.per_alpha = None
+#             self.per_beta = None
+
+#     def update(self, items: Tuple[np.ndarray, ...]):
+#         self.add_item = items
+#         states, actions, rewards, undones = items
+#         self.add_size = rewards.shape[0]
+
+#         p = self.p + self.add_size  # pointer
+#         if p > self.max_size:
+#             self.if_full = True
+#             p0 = self.p
+#             p1 = self.max_size
+#             p2 = self.max_size - self.p
+#             p = p - self.max_size
+
+#             self.states[p0:p1], self.states[0:p] = states[:p2], states[-p:]
+#             self.actions[p0:p1], self.actions[0:p] = actions[:p2], actions[-p:]
+#             self.rewards[p0:p1], self.rewards[0:p] = rewards[:p2], rewards[-p:]
+#             self.undones[p0:p1], self.undones[0:p] = undones[:p2], undones[-p:]
+#         else:
+#             self.states[self.p:p] = states
+#             self.actions[self.p:p] = actions
+#             self.rewards[self.p:p] = rewards
+#             self.undones[self.p:p] = undones
+
+#         if self.if_use_per:
+#             data_ids = np.arange(self.p, p, dtype=np.long)
+#             if p > self.max_size:
+#                 data_ids = np.mod(data_ids, self.max_size)
+
+#             for sum_tree in self.sum_trees:
+#                 sum_tree.update_ids(data_ids=data_ids, prob=10.)
+
+#         self.p = p
+#         self.cur_size = self.max_size if self.if_full else self.p
+
+#     def sample(self, batch_size: int) -> Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
+#         sample_len = self.cur_size - 1
+
+#         ids = np.random.randint(sample_len * self.num_seqs, size=(batch_size,))
+#         ids0 = np.mod(ids, sample_len)
+#         ids1 = np.floor_divide(ids, sample_len)
+
+#         return (self.states[ids0, ids1],
+#                 self.actions[ids0, ids1],
+#                 self.rewards[ids0, ids1],
+#                 self.undones[ids0, ids1],
+#                 self.states[(ids0 + 1) % self.cur_size, ids1],)
+
+#     def sample_for_per(self, batch_size: int) -> Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
+#         beg = -self.max_size
+#         end = (self.cur_size - self.max_size) if (self.cur_size < self.max_size) else -1
+
+#         is_indices = []
+#         is_weights = []
+
+#         assert batch_size % self.num_seqs == 0
+#         sub_batch_size = batch_size // self.num_seqs
+#         for env_i in range(self.num_seqs):
+#             sum_tree = self.sum_trees[env_i]
+#             _is_indices, _is_weights = sum_tree.important_sampling(batch_size, beg, end, self.per_beta)
+#             is_indices.append(_is_indices + sub_batch_size * env_i)
+#             is_weights.append(_is_weights)
+
+#         is_indices = np.hstack(is_indices)
+#         is_weights = np.hstack(is_weights)
+
+#         ids0 = np.mod(is_indices, self.cur_size)
+#         ids1 = np.floor_divide(is_indices, self.cur_size)
+#         return (
+#             self.states[ids0, ids1],
+#             self.actions[ids0, ids1],
+#             self.rewards[ids0, ids1],
+#             self.undones[ids0, ids1],
+#             self.states[(ids0 + 1) % self.cur_size, ids1],
+#             is_weights,
+#             is_indices,
+#         )
+
+#     def td_error_update_for_per(self, is_indices: np.ndarray, td_error: np.ndarray):
+#         prob = np.clip(td_error, 1e-8, 10).pow(self.per_alpha)
+
+#         batch_size = td_error.shape[0]
+#         sub_batch_size = batch_size // self.num_seqs
+#         for env_i in range(self.num_seqs):
+#             sum_tree = self.sum_trees[env_i]
+#             slice_i = env_i * sub_batch_size
+#             slice_j = slice_i + sub_batch_size
+
+#             sum_tree.update_ids(is_indices[slice_i:slice_j], prob[slice_i:slice_j])
+
+#     def save_or_load_history(self, cwd: str, if_save: bool):
+#         item_names = (
+#             (self.states, "states"),
+#             (self.actions, "actions"),
+#             (self.rewards, "rewards"),
+#             (self.undones, "undones"),
+#         )
+
+#         if if_save:
+#             for item, name in item_names:
+#                 if self.cur_size == self.p:
+#                     buf_item = item[:self.cur_size]
+#                 else:
+#                     buf_item = np.vstack((item[self.p:self.cur_size], item[0:self.p]))
+#                 file_path = f"{cwd}/replay_buffer_{name}.npy"
+#                 print(f"| buffer.save_or_load_history(): Save {file_path}")
+#                 np.save(file_path, buf_item)
+
+#         elif all([os.path.isfile(f"{cwd}/replay_buffer_{name}.npy") for item, name in item_names]):
+#             max_sizes = []
+#             for item, name in item_names:
+#                 file_path = f"{cwd}/replay_buffer_{name}.npy"
+#                 print(f"| buffer.save_or_load_history(): Load {file_path}")
+#                 buf_item = np.load(file_path)
+
+#                 max_size = buf_item.shape[0]
+#                 item[:max_size] = buf_item
+#                 max_sizes.append(max_size)
+#             assert all([max_size == max_sizes[0] for max_size in max_sizes])
+#             self.cur_size = self.p = max_sizes[0]
+#             self.if_full = self.cur_size == self.max_size
+
 class ReplayBuffer:  # for off-policy
     def __init__(self,
                  max_size: int,
